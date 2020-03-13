9c67ac3d85fe76b7d33b3841c20e82b950eff0b7
Virath <skennethchea@gmail.com>
2020-03-13 04:15:20 -0700

    modified navbar using Flexbox style model instead of box. added main contents like about me, project list, and games. Started filling in the sections with material.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Day 1:
 Created a wireframe using LucidCharts for home page. Looking for a minimalistic and fun design. 
 Started programming the home page. The navbar was created using vanilla javascript, DOM, HTML, CSS. Utilized the old <span>, class, and display: inline-block trick to create a horizontal field. 
 Some problems that occured during the process was creating an image element. Consistently kept inserting "src=" in the setAttribute() method, even though the parameters were validName: string, value: string. Another problem was that apparently javascript doesn't allow two of the same elements to be created in the same scope. Potential fix to this could be to create a new variable that holds the second similar element.  
+
+Day 2:
+Modified the CSS of navbar using Flexbox web style model. Properties were a little strange compared to the normal box model I usually use. The pros I can see so far is that it auto adjusts itself when the browser is zoomed out and in on. It might be useful with more practice. I also added the main contents into the home page; About me, Project list, Games. One messy aspect of flexbox is that it gets rid of bullets. Apparently display: flex eliminates the list styles entirely. Which is kind of a shame. Need to find a workaround for that problem. 
--- front-end/HomePage.js
+++ front-end/HomePage.js
@@ -1,33 +1,80 @@
-let navArray = ["Home", "Git Projects", "Blog Posts", "Log out"];
+let navArray = ["HOME", "GIT PROJECTS", "BLOG POSTS"];
 
 function renderNavbar(){
     
     let navbar = document.getElementById("nav-bar");
-    let span = document.createElement("span");
-    span.setAttribute("class", "nav-bar-h");
-
+    
     let logo = document.createElement("img");
     logo.setAttribute("src", "images/logo.png");
     logo.setAttribute("alt", "logo");
-    logo.setAttribute("id", "logo");
-    span.appendChild(logo);
-    navbar.appendChild(span);
+    logo.setAttribute("id", "logo")
+    navbar.appendChild(logo);
 
+    let ul = document.createElement("ul");
+    ul.setAttribute("class", "nav-bar-main");
     for(let i = 0; i < navArray.length; i++){
-        let span = document.createElement("span");
-        span.setAttribute("class", "nav-bar-h");
         let a = document.createElement("a");
         a.setAttribute("href", "/" + navArray[i].toLowerCase());
-        span.appendChild(a);
-        let p = document.createElement("p");
-        a.appendChild(p);
-        p.innerHTML = navArray[i];
-        navbar.appendChild(span);
-        if(navArray[i] === "Log out"){
-            a.setAttribute("id", "logout");
-        }
+        a.innerHTML = navArray[i];
+        ul.appendChild(a);
     }
+    navbar.appendChild(ul);
 
+    let logout = document.createElement("a");
+    logout.setAttribute("href", "/" + "logout");
+    logout.setAttribute("class", "logout");
+    logout.innerHTML = "LOG OUT";
+    navbar.appendChild(logout);
 }
 
 renderNavbar();
+
+// function renderLabel(){
+//             <span class="label-icon">
+//                 <i class="material-icons">person</i>
+//             </span>
+//             <span class="label-icon">
+//                 <label>About Me </label>
+//             </span>
+//             <div class="horizontal-line"></div>
+
+//     let spanIcon = document.createElement("span");
+//     spanIcon.setAttribute("label-icon");
+//     let i = document.createElement("i");
+//     i.setAttribute("class", "material-icons");
+//     i.innerHTML = iconArray[i];
+//     spanIcon.appendChild(i);
+//     let spanLabel = document.createElement("span");
+//     spanLabel.setAttribute("label-icon");
+//     let label = document.createElement("label");
+//     label.innerHTML = 
+// }
+
+let projectArray = {
+    0: {
+        name: "Flow Diagram V1",
+        description: `First flow diagram that I created for this project using LucidCharts. It includes features that 
+            I am currently working on, but also will be added in the future.`,
+        image: "images/flow-diagram.png"
+    },
+    1: {
+        name: "Home Page Wireframe",
+        description:"",
+        image: ""
+    }
+}
+function renderProjects(){
+    let projects = document.getElementById("get-projects");
+    let name = document.createElement("label");
+    name.innerHTML = projectArray[0].name;
+    projects.appendChild(name);
+    let description = document.createElement("p");
+    description.innerHTML = projectArray[0].description;
+    projects.appendChild(description);
+    let image = document.createElement("img");
+    image.setAttribute("src", projectArray[0].image);
+    image.setAttribute("alt", "flow-diagram");
+    image.setAttribute("id", "flow-diagram");
+    projects.appendChild(image);
+}
+renderProjects();
